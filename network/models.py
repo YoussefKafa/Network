8755eb1d33896e1ae4f0b42e04047e0d667b015e
Youssef kafa <youssef.kafa19941@gmail.com>
2022-05-17 10:05:17 -0700

    Initial commit

---
+++ network/models.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    pass
+
+class Post(models.Model):
+    text = models.CharField(max_length=255)
+    user = models.ForeignKey('User', on_delete=models.CASCADE, related_name='author')
+    date = models.DateTimeField(default=datetime.now())
+    liked = models.ManyToManyField('User', default=None, blank=True, related_name='post_likes')
+
+    @property
+    def likes_count(self):
+        return self.liked.all().count()
+
+
+class Like(models.Model):
+    user = models.ForeignKey('User', on_delete=models.CASCADE)
+    post = models.ForeignKey('Post', on_delete=models.CASCADE)
